28b542bb6836b246988749379a034c580e4c8a35
Paul Mannarino <paul.mannarino@gmail.com>
2021-01-24 22:53:03 -0500

    New headless browser == better performance

--- main.py
+++ main.py
@@ -1,43 +1,64 @@
 import configparser
 import json
+import logging
+import mechanicalsoup
 import re
+import sys
 from pushbullet import Pushbullet
 from os.path import dirname, abspath
-from selenium import webdriver 
-from selenium.webdriver.chrome.options import Options
-# import sys
+
+# Setting up file paths helps when running cron
+path = dirname(abspath(__file__))
+configPath   = f'{path}/config.ini'
+loggingPath  = f'{path}/app.log'
+productsPath = f'{path}/products.json'
+storesPath   = f'{path}/stores.json'
+
+# Log INFO level messages
+logging.basicConfig(filename=loggingPath,
+  format='%(asctime)s - %(message)s',
+  level=logging.INFO)
 # sys.exit()
 
-# Setup PushBullet
-path = dirname(abspath(__file__))
-config_path = path + '/config.ini'
+# Get PushBullet API_KEY from config
 config = configparser.ConfigParser()
-config.read(config_path)
+config.read(configPath)
+pb = Pushbullet(config['pushbullet']['API_KEY'])
 
-pb_config = config['pushbullet']
-pb = Pushbullet(pb_config['API_KEY'])
+# Load products
+with open(productsPath) as f:
+  products = json.load(f)
 
+# Load stores
+with open(storesPath) as f:
+  stores = json.load(f)
+
+def main():
+  try:
+      logging.info('___________ Begin stock check ___________')
+      browser = mechanicalsoup.StatefulBrowser(user_agent='Mozilla/5.0')
+      browser.open("https://bestbuy.com/")
+      for product in products:
+        browser.open(product['url'])
+        pageSource = str(browser.page.encode("utf-8"))
+        soldOutPhrase = stores[product["store"]]["soldOutPhrase"]
+        soldOut = re.search(soldOutPhrase, pageSource)
+        if soldOut == None:
+          pushInStockAlert(product)
+          logging.info(f'Found GPU in stock: {product["store"]}->{product["name"]}')
+        else:
+          logging.info(f'No GPU found: {product["store"]}->{product["name"]}')
+  finally:
+      try:
+          logging.info('___________ End stock check ___________')
+          browser.quit()
+      except:
+          pass
+
+      
 def pushInStockAlert(product):
   subject = f'{product["name"]} is in stock at {product["store"]}'
   pb.push_link(subject, product['url'])
 
-# Load products
-with open('./products.json') as f:
-  products = json.load(f)
-
-try:
-    opts = Options()
-    opts.headless = True
-    brower = webdriver.Chrome(options=opts)
-
-    for i in range(len(products)):
-      brower.get(products[i].get("url"))
-      pageSource = str(brower.page_source.encode("utf-8"))
-      soldOut = re.search(">Sold Out<", pageSource)
-      if soldOut == None:
-        pushInStockAlert(products[i])
-finally:
-    try:
-        brower.close()
-    except:
-        pass
+if __name__ == "__main__":
+  main()
